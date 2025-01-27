8dd68a7e5e0c28af3e15a5bec988be66c1dd4a44
ydj515 <ydj515@hanmail.net>
2025-01-27 22:53:06 +0900

    refactor: DateTimeUtilsTest kotlin으로 전환

--- src/test/java/com/makers/princemaker/util/DateTimeUtilsTest.kt
+++ src/test/java/com/makers/princemaker/util/DateTimeUtilsTest.kt
@@ -1,19 +1,16 @@
-package com.makers.princemaker.util;
+package com.makers.princemaker.util
 
-import org.junit.jupiter.api.Test;
-
-import java.time.LocalDateTime;
-
-import static com.makers.princemaker.util.DateTimeUtilsKt.getLocalDateTimeString;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
 
 class DateTimeUtilsTest {
     @Test
-    void getLocalDateTimeStringTest() {
-        String result = getLocalDateTimeString(
-                LocalDateTime.of(2023, 12, 21, 10, 10)
-        );
+    fun localDateTimeStringTest() {
+        val result = getLocalDateTimeString(
+            LocalDateTime.of(2023, 12, 21, 10, 10)
+        )
 
-        assertEquals("2023-12-21 탄생", result);
+        Assertions.assertEquals("2023-12-21 탄생", result)
     }
 }
